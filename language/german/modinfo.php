590f7920688c59d5889af199711b22720ed35186
mambax7 <mambax7@gmail.com>
2020-10-25 03:38:58 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ language/german/modinfo.php
@@ -0,0 +1,5 @@
+<?php
+
+// Module Info
+define('_XF_XFACCOUNT_DESC', 'View all information about a single account/user');
+define('_XF_XFACCOUNT_NAME', 'My Personal Page');
---
+++ language/pt_utf8/modinfo.php
@@ -0,0 +1,9 @@
+<?php
+
+// Module Info
+define('_XF_XFACCOUNT_NAME', 'Minha Conta');
+define('_XF_XFACCOUNT_DESC', 'Mostrar todas as informações de um associado');
+define('_XF_ACCOUNT_PERSONAL', 'Página pessoal');
+define('_XF_ACCOUNT_DIARY', 'Diário e Notas');
+define('_XF_ACCOUNT_SKILLS', 'My Skills Profile');
+define('_XF_ACCOUNT_PUBKEYS', 'My Public Keys');
